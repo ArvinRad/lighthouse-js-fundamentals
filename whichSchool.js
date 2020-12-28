3fd081807f06395b54a0d0b43143f22d3aec909f
ArvinRad <radanarvin@gmail.com>
2020-12-28 21:23:20 +0000

    Use if to decide the School typefrom age

---
+++ whichSchool.js
@@ -0,0 +1,5 @@
+var age=10;
+console.log("I am "+age+", Which school should I go to?");
+whichSchool=((13<=age)&&(age<=18) ? "Secondary School" : (age<13 ? "Elementary School" : "Lighthouse Labs"));
+console.log(whichSchool);
+function
